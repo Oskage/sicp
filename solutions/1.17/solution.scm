1031b6e2f5492b9d9a09e2dd2699c291d2674b59
Oskage <oskage.work@gmail.com>
2023-06-21 18:45:30 +0300

    add solution for 1.17

---
+++ solutions/1.17/solution.scm
@@ -0,0 +1,20 @@
+(define (even? x) 
+    (= (remainder x 2) 0))
+
+(define (fast-mult a b)
+    (cond ((= b 0) 0)
+          ((even? b) (fast-mult (+ a a) (/ b 2)))
+          (else (+ a (fast-mult a (- b 1))))))
+
+(newline)
+(display "Enter a number: ")
+(define a (read))
+(display "Enter a multiplier: ")
+(define b (read))
+
+(display a)
+(display " * ")
+(display b)
+(display " = ")
+(display (fast-mult a b))
+(newline)
